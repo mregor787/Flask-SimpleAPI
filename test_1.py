4cacedf622addf7517588baccf69d72c06717c78
Egor Belousov <egor0288354@yandex.ru>
2021-04-08 22:07:37 +0300

    Add get jobs tests

--- test_1.py
+++ test_1.py
@@ -1,13 +1,6 @@
 from requests import get
 
-# Получение всех работ:
-print(get(...).json())
-
-# Корректное получение одной работы
-print(...)
-
-# Ошибочный запрос на получение одной работы - неверный id
-print(...)
-
-# Ошибочный запрос на получение одной работы - строка вместо id
-print(...)
+print(get('http://127.0.0.1:8080/api/jobs').json())  # get all jobs
+print(get('http://127.0.0.1:8080/api/jobs/1').json())  # get single job (valid)
+print(get('http://127.0.0.1:8080/api/jobs/999').json())  # get single job (invalid - not found id)
+print(get('http://127.0.0.1:8080/api/jobs/q'))  # get single job (invalid - string instead of int)
