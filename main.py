a5f66140d53a41ad3a39968bef925a32f79c01d5
Egor Belousov <egor0288354@yandex.ru>
2021-04-08 22:09:32 +0300

    Add host & port for app runner

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 def main():
     db_session.global_init('db/mars_db.sqlite')
     app.register_blueprint(jobs_api.blueprint)
-    app.run()
+    app.run(host='127.0.0.1', port=8080)
